45e872b9ea23e8ab356f9d9762883294a4fa3916
Joe Evans <joeev@amazon.com>
2020-01-23 19:01:58 -0800

    Add batch file to pass platform-specific paths in to jenkins slave runner.

---
+++ windows/run-auto-connect.bat
@@ -0,0 +1,5 @@
+cd C:\
+
+C:\Python37\python.exe .\slave-autoconnect.py --slave-name-file=C:\jenkins_slave\jenkins_slave_name.txt --master-file=C:\jenkins_slave\jenkins_master_url.txt --master-private-file=C:\jenkins_slave\jenkins_master_private_url.txt
+
+
