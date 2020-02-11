57568957c18d58ef216a7798b4ca1db489c1cc32
Ubuntu <ubuntu@ip-172-31-8-137.us-east-2.compute.internal>
2020-02-11 08:32:33 +0000

    TVM build dependencies

--- windows/windows_deps_headless_installer.py
+++ windows/windows_deps_headless_installer.py
@@ -52,6 +52,8 @@ DEPS = {
         'opencv': 'https://windows-post-install.s3-us-west-2.amazonaws.com/opencv-windows-4.1.2-vc14_vc15.zip',
         'cudnn': 'https://windows-post-install.s3-us-west-2.amazonaws.com/cudnn-9.2-windows10-x64-v7.4.2.24.zip',
         'nvdriver': 'https://windows-post-install.s3-us-west-2.amazonaws.com/nvidia_display_drivers_398.75_server2016.zip',
+        'perl': 'http://strawberryperl.com/download/5.30.1.1/strawberry-perl-5.30.1.1-64bit.msi',
+        'ninja': 'https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-win.zip',
         # This installation of CMake breaks windows PATH when executing vcvars, installing from
         # chocolatey from powershell instead.
         'cmake': 'https://github.com/Kitware/CMake/releases/download/v3.16.2/cmake-3.16.2-win64-x64.msi'
@@ -227,6 +229,22 @@ def install_cmake():
     logging.info("CMAKE install complete")
 
 
+def install_perl():
+    logging.info("Installing Perl")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        perl_file_path = download(DEPS['perl'], tmpdir)
+        check_call(['msiexec ', '/n', '/passive', '/i', perl_file_path])
+    logging.info("Perl install complete")
+
+
+def install_ninja():
+    logging.info("Installing Ninja")
+    local_file = download(DEPS['ninja'])
+    with zipfile.ZipFile(local_file, 'r') as zip:
+        zip.extractall("C:\\Program Files\\ninja-win")
+    logging.info("Ninja Install complete")
+
+
 def install_openblas():
     logging.info("Installing OpenBLAS")
     local_file = download(DEPS['openblas'])
@@ -324,7 +342,7 @@ def add_paths():
     logging.info("Adding Windows Kits to PATH...")
     current_path = run_command("PowerShell (Get-Itemproperty -path 'hklm:\\system\\currentcontrolset\\control\\session manager\\environment' -Name Path).Path")
     logging.debug("current_path: {}".format(current_path))
-    new_path = current_path + ";C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.16299.0\\x86;C:\\Program Files\\OpenBLAS-windows-v0_2_19\\bin"
+    new_path = current_path + ";C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.16299.0\\x86;C:\\Program Files\\OpenBLAS-windows-v0_2_19\\bin;C:\\Program Files\\ninja-win;C:\\Strawberry\\perl\\bin"
     logging.debug("new_path: {}".format(new_path))
     run_command("PowerShell Set-ItemProperty -path 'hklm:\\system\\currentcontrolset\\control\\session manager\\environment' -Name Path -Value '" + new_path + "'")
 
@@ -356,7 +374,7 @@ def main():
                         default=False,
 			action='store_true')
     args = parser.parse_args()
-    if args.gpu or has_gpu():
+    if args.gpu and has_gpu():
         logging.info("GPU detected")
         install_nvdriver()
         install_cuda()
@@ -368,6 +386,8 @@ def main():
     install_openblas()
     install_mkl()
     install_opencv()
+    install_perl()
+    install_ninja()
     add_paths()
 
 
